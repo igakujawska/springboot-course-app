d9fdfe1007619d2cdb362157a74e76a5b2669b06
iga <igakuj1@gmail.com>
2026-09-15 12:19:42 +0200

    adding users

--- src/main/java/com/example/springbootkursapp/controller/UserController.java
+++ src/main/java/com/example/springbootkursapp/controller/UserController.java
@@ -6,5 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //kontrolery do odbierania żądań,, ustawienie adnotacji
 public class UserController {
     @GetMapping("/users") //przypisanie, zmapowanie prostego endpointu do kontrolera
-    public String getUsers() {return "Lista użytkowników: ";}
+    public List<User> getUsers()
+    {
+        List<User> users = new ArrayList<>();
+
+        users.add(new User(1,"admin","admin","<EMAIL>"));
+        users.add(new User(2,"user","user","<EMAIL>"));
+        users.add(new User(3, "user2", "user2", "<EMAIL>"));
+
+        return users;
+    }
 }
