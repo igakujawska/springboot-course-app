45fda3f6138310b935a2e3a9805549ea539c0adb
iga <igakuj1@gmail.com>
2026-09-15 10:48:56 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootkursapp/SpringbootKursAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkursapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKursAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKursAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootkursapp/SpringbootKursAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkursapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKursAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
