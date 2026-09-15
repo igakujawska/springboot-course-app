b15bf4ed7ef38b4587bb7f3cd6e8c15946623846
iga <igakuj1@gmail.com>
2026-09-15 14:17:23 +0200

    added @PostMapping

--- src/main/java/com/example/springbootkursapp/controller/UserController.java
+++ src/main/java/com/example/springbootkursapp/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.springbootkursapp.model.User;
 import com.example.springbootkursapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -24,5 +26,11 @@ public class UserController {
     public List<User> getUsers()
     {
         return userRepository.findAll(); //mozemy uzyc funkcji, bo mamy juz repo
+    }
+
+    @PostMapping("/users")
+    public User addUser(@RequestBody User user)
+    {
+        return userRepository.save(user);
     }
 }
