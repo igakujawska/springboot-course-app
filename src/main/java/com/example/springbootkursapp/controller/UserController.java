0b6783ef41bf6580141eec3674e06e287184e84a
iga <igakuj1@gmail.com>
2026-09-15 12:42:26 +0200

    adding imports

--- src/main/java/com/example/springbootkursapp/controller/UserController.java
+++ src/main/java/com/example/springbootkursapp/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.example.springbootkursapp.controller;
 
+import com.example.springbootkursapp.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController //kontrolery do odbierania żądań,, ustawienie adnotacji
 public class UserController {
