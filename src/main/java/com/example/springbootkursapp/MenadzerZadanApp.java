1b7aa16fb85c07fb8420de81f00558f4431361ea
iga <igakuj1@gmail.com>
2026-09-15 11:46:46 +0200

    renaming app

--- src/main/java/com/example/springbootkursapp/MenadzerZadanApp.java
+++ src/main/java/com/example/springbootkursapp/MenadzerZadanApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootKursAppApplication {
+public class MenadzerZadanApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootKursAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MenadzerZadanApp.class, args);
+    }
 
 }
