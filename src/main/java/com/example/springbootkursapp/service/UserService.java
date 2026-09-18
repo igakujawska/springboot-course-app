61b07ac91e752a940d3cfc024217f5bfd510b503
iga <igakuj1@gmail.com>
2026-09-18 13:24:52 +0200

    added UserService and @Service, @Bean

--- src/main/java/com/example/springbootkursapp/service/UserService.java
+++ src/main/java/com/example/springbootkursapp/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.springbootkursapp.model.User;
 import com.example.springbootkursapp.repository.UserRepository;
 import jakarta.websocket.server.ServerEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
