8a6a2c3d552eacb270623cd168b6df8d9de4fa76
iga <igakuj1@gmail.com>
2026-09-22 14:31:46 +0200

    fixed userservice code

--- src/main/java/com/example/springbootkursapp/service/UserService.java
+++ src/main/java/com/example/springbootkursapp/service/UserService.java
@@ -13,33 +13,32 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService implements UserDetailsService
+{
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder; //interfejs do zamiany/enkodowania hasla
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder)
+    {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public List<User> getUsers() {
+    public List<User> getUsers()
+    {
         return userRepository.findAll();
     }
-        public User addUser(User user)
-        { //@RequestBody wystepuje tylko w kontrolerze, nie serwisie
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            return userRepository.save(user);
-        }
+
+    public User addUser(User user)
+    { //@RequestBody wystepuje tylko w kontrolerze, nie serwisie
+         user.setPassword(passwordEncoder.encode(user.getPassword()));
+         return userRepository.save(user);
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
     {
-        return null;
+        return userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("User not found"));
     }
-}
-@Override
-public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-    return userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("User not found"));
-}
 }
